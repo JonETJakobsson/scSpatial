97e73270cd8e76a000718c574a89f9880d681e4f
Jon Jakobsson <33955709+JonETJakobsson@users.noreply.github.com>
2022-01-31 11:18:46 +0100

    Added a dataset class that can accept images and gene expression data

--- test_io.py
+++ test_io.py
@@ -1,4 +1,4 @@
-from scSpatial import io
+from scSpatial import utility
 
 # def test_select_file():
 #     path = io.select_file()
@@ -6,6 +6,6 @@ from scSpatial import io
 
 
 def test_open_and_view_image_in_napari() -> None:
-    io.open_image("Dapi") # starts a napari window, select Dapi or any other channel
+    utility.open_image("Dapi") # starts a napari window, select Dapi or any other channel
 
 
