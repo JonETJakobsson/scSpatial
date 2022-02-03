ad89c22f0160bc2476ccead874de23e0769fccbd
Jon Jakobsson <33955709+JonETJakobsson@users.noreply.github.com>
2022-02-03 16:40:22 +0100

    added a map gene method to the segmentation class

--- scSpatial/main.py
+++ scSpatial/main.py
@@ -16,12 +16,12 @@ df = d1.gene_expression
 
 idx = list()
 for i, gene in df.iterrows():
-    if gene.PosX > 6000 and gene.PosX <= 8000:
-        if gene.PosY > 5000 and gene.PosY <=6000:
+    if gene.x > 6000 and gene.x <= 8000:
+        if gene.y > 5000 and gene.y <= 6000:
             idx.append(i)
 df = df.iloc[idx]
-df.PosX = df.PosX-6000
-df.PosY = df.PosY-5000
+df.x = df.x - 6000
+df.y = df.y - 5000
 df
 d1.images["Cytoplasm"] = d1.images["Cytoplasm"][5000:6000, 6000:8000]
 
@@ -33,8 +33,10 @@ d1.run_segmentation(seg)
 viewer = napari.Viewer(axis_labels=["Dorsoventral", "Mediolateral"])
 
 viewer.add_image(d1.images["Nuclei"], name="Nuclei", colormap="yellow")
-viewer.add_image(d1.images["Cytoplasm"], name="Cytoplasm", blending="additive", colormap="cyan")
-viewer.add_points(data=list(zip(df.PosY, df.PosX)), properties=df, text="Gene")
+viewer.add_image(
+    d1.images["Cytoplasm"], name="Cytoplasm", blending="additive", colormap="cyan"
+)
+viewer.add_points(data=list(zip(df.y, df.x)), properties=df, text="Gene")
 
 viewer.add_labels(d1.segmentation.objects)
 napari.run()
