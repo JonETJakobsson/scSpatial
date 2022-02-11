643adcf1288f552794f83edafcad85437279fea1
KajsaMagnusson <98471143+KajsaMagnusson@users.noreply.github.com>
2022-02-11 16:34:45 +0100

    Update kajsa.py

--- scSpatial/kajsa.py
+++ scSpatial/kajsa.py
@@ -1,3 +1,4 @@
+
 from configparser import MAX_INTERPOLATION_DEPTH
 from email.charset import QP
 from hashlib import sha1
@@ -7,7 +8,7 @@ import sys
 import this
 from tkinter import dialog
 from urllib import response
-from PyQt5.QtWidgets import QWidget, QLabel, QListWidget, QVBoxLayout, QHBoxLayout, QPushButton, QApplication, QLineEdit, QTabWidget, QGridLayout, QVBoxLayout, QFileDialog, QComboBox
+from PyQt5.QtWidgets import QWidget, QLabel, QListWidget, QVBoxLayout, QHBoxLayout, QPushButton, QApplication, QLineEdit, QTabWidget, QGridLayout, QVBoxLayout, QFileDialog, QComboBox, QFormLayout
 from PyQt5.QtGui import QFont
 from click import option
 from matplotlib.pyplot import show
@@ -18,72 +19,49 @@ class kajsasWidget(QWidget):
     def __init__(self):
         super().__init__()
         self.resize(500, 200)
-# self.dataset = dataset
-# self.viewer = viewer
         self.initUI()
 
     def initUI(self):
-
-        self.label = QLabel(self)
-        self.label.setText("Select file from drop down menu:") #Kan jag fixa så detta får plats?
-        self.label.setFont(h1)
-       
-        # Test file dialog
-        layout = QVBoxLayout()
+        
+        layout = QFormLayout()
         self.setLayout(layout)
 
-        self.options = ('Select a nuclei image', 'Select a cytoplasm image', 'Select a gene expression file')
+        btn_nuclei = QPushButton('Select a nuclei image')
+        btn_nuclei.clicked.connect(self.launchNucleiDialog)
+        layout.addWidget(btn_nuclei)
 
-        self.combo = QComboBox()
-        self.combo.addItems(self.options)
-        layout.addWidget(self.combo)
+        btn_cytoplasm = QPushButton('Select a cytoplasm image')
+        btn_cytoplasm.clicked.connect(self.launchCytoplasmDialog)
+        layout.addWidget(btn_cytoplasm)
 
-        btn = QPushButton('Load')
-        btn.clicked.connect(self.launchDialog)
-        layout.addWidget(btn)
+        btn_other = QPushButton('Other channel')
+        btn_other.clicked.connect(self.launchOtherDialog)
+        layout.addWidget(btn_other)
 
-    def launchDialog(self):
-        option = self.options.index(self.combo.currentText())
+        self.label = QLabel(self)
 
-        if option == 0:
-            response = self.SelectaNucleiImage()
-        elif option == 1:
-            response = self.SelectaCytoplasmImage()
-        elif option == 2:
-            option = self.SelectaGeneExpressionFile()
-        else:
-            print('Got nothing')
+    def launchNucleiDialog(self):
+        
+        fname = QFileDialog.getOpenFileName(self, 'Select a nuclei image', '')
+
+        if fname:
+            self.label.setText(fname[0])
+
+    def launchCytoplasmDialog(self):
+        
+        fname = QFileDialog.getOpenFileName(self, 'Select a cytoplasm image', '')
+
+        if fname:
+            self.label.setText(fname[0])
+
+    def launchOtherDialog(self):
+        
+        fname = QFileDialog.getOpenFileName(self, 'Select other channel', '')
+
+        if fname:
+            self.label.setText(fname[0])
     
-    def SelectaNucleiImage(self):
 
-        response = QFileDialog.getOpenFileName(
-            parent=self,
-            caption='Please select a nuclei image',
-            directory=os.getcwd()
-        
-        )
-        print(response)
-
-    def SelectaCytoplasmImage(self):
-
-        response = QFileDialog.getOpenFileName(
-            parent=self,
-            caption='Please select a cytoplasm image',
-            directory=os.getcwd()
-        
-        )
-        print(response)
-
-    def SelectaGeneExpressionFile(self):
-
-        response = QFileDialog.getOpenFileName(
-            parent=self,
-            caption='Please select a gene expression file',
-            directory=os.getcwd()
-        
-        )
-        print(response)
-    
 app = QApplication(sys.argv)
 
 demo = kajsasWidget()
