8fc81252a9592e5416c7f6f121bbd68ac08d44cb
Jon Jakobsson <33955709+JonETJakobsson@users.noreply.github.com>
2022-02-11 11:36:38 +0100

    rewrote loadGeneWidget to subsequently add UI elements

--- scSpatial/main_widget.py
+++ scSpatial/main_widget.py
@@ -1,9 +1,9 @@
 import sys
 from PyQt5.QtWidgets import QWidget, QTabWidget, QApplication, QVBoxLayout
 from PyQt5.QtGui import QFont
-
-# from kajsa import kajsasWidget
-from widgets import geneColumnSelectWidget, colorObjectWidget
+from widgets import loadGenesWidget
+from dataset import Dataset
+from napari import Viewer
 
 
 h1 = QFont("Arial", 13)
@@ -11,7 +11,7 @@ h1 = QFont("Arial", 13)
 
 # Main widget presents the tabs and can provide other global tools
 class mainWidget(QWidget):
-    def __init__(self, dataset, viewer):
+    def __init__(self, dataset: Dataset, viewer: Viewer):
         super().__init__()
         self.dataset = dataset
         self.viewer = viewer
@@ -24,8 +24,7 @@ class mainWidget(QWidget):
 
         # Create tabs with names to access induvidual widgets
         self.tabs = QTabWidget()
-        # self.tabs.addTab(kajsasWidget, "kajsa")
-        self.tabs.addTab(geneColumnSelectWidget(self.dataset, self.viewer), "select columns")
+        self.tabs.addTab(loadGenesWidget(self.dataset, self.viewer), "select columns")
 
         # Add all widgets in order
         layout.addWidget(self.tabs)
--- scSpatial/widgets.py
+++ scSpatial/widgets.py
@@ -1,3 +1,4 @@
+from re import X
 from PyQt5.QtWidgets import (
     QWidget,
     QLabel,
@@ -12,49 +13,109 @@ from PyQt5.QtWidgets import (
 )
 from PyQt5.QtGui import QFont
 import sys
-import os
+from dataset import Dataset
+from napari import Viewer
 
 
 h1 = QFont("Arial", 13)
 
 
-class geneColumnSelectWidget(QWidget):
-    def __init__(self, dataset, viewer):
+class loadGenesWidget(QWidget):
+    def __init__(self, dataset: Dataset, viewer: Viewer):
         super().__init__()
         self.dataset = dataset
         self.viewer = viewer
         self.initUI()
 
     def initUI(self):
-
-        btn = QPushButton("select gene expression file")
-        btn.clicked.connect(self.load_df)
-
-        columns = ["x", "y", "other", "gene"]
-
-        self.list_x = QComboBox()
-        self.list_x.addItems(columns)
-        self.list_y = QComboBox()
-        self.list_y.addItems(columns)
-        self.list_gene = QComboBox()
-        self.list_gene.addItems(columns)
-
-        layout = QFormLayout()
-        layout.addWidget(btn)
-        layout.addRow("x", self.list_x)
-        layout.addRow("y", self.list_y)
-        layout.addRow("gene", self.list_gene)
-        layout.addWidget(btn)
-
-        self.setLayout(layout)
+        # Button to open file dialog
+        self.btn_load_file = QPushButton("Select gene file")
+        self.btn_load_file.clicked.connect(self.load_df)
+      
+        # Use form layout and add button to the top
+        self.layout = QFormLayout()
+        self.layout.addWidget(self.btn_load_file)
+        
+        # Set the layout to the loadGenesWidget
+        self.setLayout(self.layout)
 
     def load_df(self):
+        """Runns the file dialog and add more elements to the 
+        UI for selecting correct columns"""
+
         import pandas as pd
+
+        # Fetch path to file, note that this return a 
+        # tuple and path is on index 0
         path = QFileDialog.getOpenFileName(
             self,
-            caption="select a gene expression file")
-        self.df = pd.read_csv(path)
-        print(self.df)
+            caption="select a gene expression file"
+        )
+        
+        # read in the gene expression file into df
+        # and also store columns in a variable
+        self.df = pd.read_csv(path[0])
+        self.columns = self.df.columns
+
+        # Create the selection widgets and add the columns options
+        self.list_x = QComboBox()
+        self.list_y = QComboBox()
+        self.list_gene = QComboBox()
+        self.list_x.addItems(self.columns)
+        self.list_y.addItems(self.columns)
+        self.list_gene.addItems(self.columns)
+
+        # If we recognize the columns, preset the values
+        if "PosX" in self.columns and "PosY" in self.columns and "Gene" in self.columns:
+            self.list_x.setCurrentText("PosX")
+            self.list_y.setCurrentText("PosY")
+            self.list_gene.setCurrentText("Gene")
+
+        
+        # Add the widgets to the form layout
+        self.layout.addRow("x", self.list_x)
+        self.layout.addRow("y", self.list_y)
+        self.layout.addRow("gene", self.list_gene)
+        
+
+        # Add a confirmation button to the bottom of the UI
+        self.btn_confirm_columns = QPushButton("Confirm")
+        self.btn_confirm_columns.clicked.connect(self.save_df_to_dataset)
+        self.layout.addWidget(self.btn_confirm_columns)
+        
+
+    def save_df_to_dataset(self):
+        """Renames the columns of the gene expression dataframe
+        and stores it in the dataset. Furthermore adds these to the viewer"""
+        #Find file representation of columns
+        x = self.list_x.currentText()
+        y = self.list_y.currentText()
+        gene = self.list_gene.currentText()
+
+        # Filter on x y and gene
+        self.df = self.df[[x, y, gene]]
+        # Rename columns to standardized names
+        self.df.columns = ["x", "y", "gene"]
+
+        # Save gene expression to the dataset
+        self.dataset.gene_expression = self.df
+
+        self.btn_confirm_columns.setVisible(False)
+
+        # Load gene into the viewer
+        # TODO I would like to see all functions adding data to the viewer 
+        # somewhere else (together), as it makes it easier to change the apearance of the app
+        # Now we have to look through random widgets to change these things.
+        self.viewer.add_points(
+            data=list(zip(
+                self.dataset.gene_expression.x,
+                self.dataset.gene_expression.y
+            )),
+            properties=self.dataset.gene_expression,
+            text="gene",
+            name="Genes",
+        )
+
 
 
 
