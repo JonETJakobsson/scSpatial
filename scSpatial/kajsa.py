868394d2e3d56ec5218c8673433ac63de5d4d648
KajsaMagnusson <98471143+KajsaMagnusson@users.noreply.github.com>
2022-02-14 10:33:58 +0100

    Update kajsa.py

--- scSpatial/kajsa.py
+++ scSpatial/kajsa.py
@@ -8,10 +8,11 @@ import sys
 import this
 from tkinter import dialog
 from urllib import response
-from PyQt5.QtWidgets import QWidget, QLabel, QListWidget, QVBoxLayout, QHBoxLayout, QPushButton, QApplication, QLineEdit, QTabWidget, QGridLayout, QVBoxLayout, QFileDialog, QComboBox, QFormLayout
+from PyQt5.QtWidgets import QWidget, QLabel, QListWidget, QVBoxLayout, QHBoxLayout, QPushButton, QApplication, QLineEdit, QTabWidget, QGridLayout, QVBoxLayout, QFileDialog, QComboBox, QFormLayout, QDialogButtonBox
 from PyQt5.QtGui import QFont
 from click import option
 from matplotlib.pyplot import show
+from torch import layout
 
 h1 = QFont("Arial", 13)
 
@@ -23,48 +24,54 @@ class kajsasWidget(QWidget):
 
     def initUI(self):
         
-        layout = QFormLayout()
+        layout = QGridLayout()
         
-
         btn_nuclei = QPushButton('Select a nuclei image')
         btn_nuclei.clicked.connect(self.launchNucleiDialog)
-        layout.addWidget(btn_nuclei)
-        self.label_nuc = QLabel("Please select nuclei file")
-        layout.addWidget(self.label_nuc)
+        layout.addWidget(btn_nuclei, 0, 0)
 
         btn_cytoplasm = QPushButton('Select a cytoplasm image')
         btn_cytoplasm.clicked.connect(self.launchCytoplasmDialog)
-        layout.addWidget(btn_cytoplasm)
+        layout.addWidget(btn_cytoplasm, 1, 0)
 
         btn_other = QPushButton('Other channel')
         btn_other.clicked.connect(self.launchOtherDialog)
-        layout.addWidget(btn_other)
+        layout.addWidget(btn_other, 2, 0)
+        
+        self.label_nuc = QLabel("")
+        layout.addWidget(self.label_nuc, 0, 1)
 
-        self.label = QLabel("Please select a file")
-        layout.addWidget(self.label)
+        self.label_cyto = QLabel("")
+        layout.addWidget(self.label_cyto, 1, 1)
+
+        self.label_other = QLabel("")
+        layout.addWidget(self.label_other, 2, 1)
 
         self.setLayout(layout)
 
     def launchNucleiDialog(self):
         
-        fname = QFileDialog.getOpenFileName(self, 'Select a nuclei image', '')
+        file = QFileDialog.getOpenFileName(self, 'Select a nuclei image', '')[0]
+        fname = file.split("/")[-1]
 
-        if fname:
-            self.label_nuc.setText(fname[0])
+        if file:
+            self.label_nuc.setText(fname)
 
     def launchCytoplasmDialog(self):
         
-        fname = QFileDialog.getOpenFileName(self, 'Select a cytoplasm image', '')
+        file = QFileDialog.getOpenFileName(self, 'Select a cytoplasm image', '')[0]
+        fname = file.split("/")[-1]
 
-        if fname:
-            self.label.setText(fname[0])
+        if file:
+            self.label_cyto.setText(fname)
 
     def launchOtherDialog(self):
         
-        fname = QFileDialog.getOpenFileName(self, 'Select other channel', '')
+        file = QFileDialog.getOpenFileName(self, 'Select other channel', '')[0]
+        fname = file.split("/")[-1]
 
-        if fname:
-            self.label.setText(fname[0])
+        if file:
+            self.label_other.setText(fname)
     
 
 app = QApplication(sys.argv)
